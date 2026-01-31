a7bbdaf200da8d8febf7d872182d9e97d33f78cd
Robert Pelloni <pelloni.robert@gmail.com>
2026-01-31 11:58:36 -0500

    feat: implement Squad Command UI and refactor squad router

--- packages/core/src/routers/squadRouter.ts
+++ packages/core/src/routers/squadRouter.ts
@@ -1,34 +1,35 @@
-import { z } from 'zod';
-import { t, publicProcedure } from '../lib/trpc-core.js';
 
-export const squadRouter = t.router({
-    list: publicProcedure.query(() => {
+import { z } from 'zod';
+import { publicProcedure, router } from '../trpc.js';
+
+export const squadRouter = router({
+    list: publicProcedure.query(async ({ setHeaders }) => {
         // @ts-ignore
-        if (global.mcpServerInstance && global.mcpServerInstance.squadService) {
-            // @ts-ignore
-            return global.mcpServerInstance.squadService.listMembers();
-        }
-        return [];
+        const server = global.mcpServerInstance;
+        if (!server) return [];
+        return server.squadService.listMembers();
     }),
-    spawn: publicProcedure.input(z.object({
-        branch: z.string(),
-        goal: z.string()
-    })).mutation(async ({ input }) => {
-        // @ts-ignore
-        if (global.mcpServerInstance && global.mcpServerInstance.squadService) {
+
+    spawn: publicProcedure
+        .input(z.object({
+            branch: z.string(),
+            goal: z.string()
+        }))
+        .mutation(async ({ input }) => {
             // @ts-ignore
-            return await global.mcpServerInstance.squadService.spawnMember(input.branch, input.goal);
-        }
-        throw new Error("SquadService not found");
-    }),
-    kill: publicProcedure.input(z.object({
-        branch: z.string()
-    })).mutation(async ({ input }) => {
-        // @ts-ignore
-        if (global.mcpServerInstance && global.mcpServerInstance.squadService) {
+            const server = global.mcpServerInstance;
+            if (!server) throw new Error("Server not initialized");
+            return await server.squadService.spawnMember(input.branch, input.goal);
+        }),
+
+    kill: publicProcedure
+        .input(z.object({
+            branch: z.string()
+        }))
+        .mutation(async ({ input }) => {
             // @ts-ignore
-            return await global.mcpServerInstance.squadService.killMember(input.branch);
-        }
-        throw new Error("SquadService not found");
-    })
+            const server = global.mcpServerInstance;
+            if (!server) throw new Error("Server not initialized");
+            return await server.squadService.killMember(input.branch);
+        })
 });
